13155d193a587ee32e69e450e46808daa95a10b9
Riya Thakur <riyathakur155555@gmail.com>
2026-07-14 02:53:20 +0530

    fix: declare explicit exports for three namespace to bypass type errors

--- three-declarations.d.ts
+++ three-declarations.d.ts
@@ -1,2 +1,28 @@
-declare module 'three';
-declare module 'postprocessing';
+declare module 'three' {
+  export const Vector2: any;
+  export type Vector2 = any;
+  export const Vector3: any;
+  export type Vector3 = any;
+  export const Scene: any;
+  export type Scene = any;
+  export const WebGLRenderer: any;
+  export type WebGLRenderer = any;
+  export const PerspectiveCamera: any;
+  export type PerspectiveCamera = any;
+  export const ShaderMaterial: any;
+  export type ShaderMaterial = any;
+  export const PlaneGeometry: any;
+  export type PlaneGeometry = any;
+  export const Mesh: any;
+  export type Mesh = any;
+  export const Clock: any;
+  export type Clock = any;
+}
+
+declare module 'postprocessing' {
+  export const BloomEffect: any;
+  export const ChromaticAberrationEffect: any;
+  export const EffectComposer: any;
+  export const EffectPass: any;
+  export const RenderPass: any;
+}
